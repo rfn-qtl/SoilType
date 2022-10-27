50bd4d17e3a8b8c439c228828338e5faf61458a9
Roberto Fritsche-Neto <110871430+rfn-qtl@users.noreply.github.com>
2022-10-27 11:31:48 -0500

    Add files via upload

---
+++ R/get_soil_plot_level.R
@@ -0,0 +1,99 @@
+#' Predict  soil characteristics at plot level
+#'
+#' This function, from a couple of samples in a trial, predict  soil characteristics at plot level via Random Forest.
+#' @param samples data.frame The first columns must be sample_id, lat and long.
+#' The other columns are the soil traits to be extrapolated/predicted at plot level.
+#' The minimum number of samples is 5.
+#' @param plot.polygons Shapefile/polygons with the id, lat, and long plots information. The coordinate system must be the same for samples and the shapefile.
+#' @return data.frame The output is a data.frame with the plot IDs, their lat and long coordinates, the number of samples used to make predictions, the RMSE and R-square for each prediction, the trait predicted, and the predicted value.
+#' @examples
+#' loading some soil samples
+#' samples <- data.table::fread(system.file("soil_data", "soil_samples.txt", package = "SoilType"))
+#' head(samples)
+
+#' loading the respectively shapefile
+#' library(raster)
+#' plot.polygons <- shapefile(system.file("soil_data", "PI_R1_DAS.shp", package = "SoilType"))
+#' length(plot.polygons) #number of features (plots)
+#' head(plot.polygons@data, 6)
+#' plot.polygons[1,]@bbox
+
+#' Running the function
+#' test3 <- get_soil_plot_level(samples = samples, plot.polygons = plot.polygons)
+
+#' and take a look at the results and the average KPI per soil trait
+#' head(test3)
+#' tapply(test3$Rsquared, test3$Trait, mean)
+
+#' @export
+get_soil_plot_level <- function(samples,  plot.polygons){
+
+  # extracting plot geo information
+  plot.coord <- data.frame()
+  for(i in 1:length(plot.polygons)){
+  plot.coord <- rbind(plot.coord, data.frame(
+    plot_id = i,
+    lat = mean(plot.polygons[i,]@bbox[1,]) / 10000,
+    long = mean(plot.polygons[i,]@bbox[2,]) /100000
+  ))
+  }
+
+  # melting by trait
+  samples.melted <- reshape2::melt(samples, id.vars = colnames(samples)[1:3])
+  samples.melted$lat <- samples.melted$lat/10000
+  samples.melted$long <- samples.melted$long/100000
+
+  # check points
+  unique.samples <- unique(samples.melted$sample)
+  unique.lat <- unique(samples.melted$lat)
+  unique.long <- unique(samples.melted$long)
+  unique.values <- unique(samples.melted$value)
+  if(anyNA(unique.samples) | length(unique.samples) <= 5) stop("The dataset must have more than 5 samples")
+  if(anyNA(unique.lat) | is.numeric(unique.lat) == F | all(unique.lat < -90) | all(unique.lat > 90)) stop("The lat must numeric, and between -90 and 90)")
+  if(anyNA(unique.long) | is.numeric(unique.long) == F | all(unique.long < -180) | all(unique.long > 180)) stop("The long must numeric, and between -180 and 180)")
+  if(anyNA(unique.values) | is.numeric(unique.values) == F) stop("The values must numeric, and NA are not allowed)")
+
+# index for traits
+traits <- unique(samples.melted$variable)
+
+# retrieving many samples in parallel
+require(foreach)
+require(doParallel)
+require(doMC)
+# setting the number of cores that will be used
+registerDoParallel(cores = detectCores()) # type the number of cores you want to use
+
+out.plot.level <- foreach(i = 1:length(traits),
+                 .packages = c("caret"),
+                 .combine = "rbind",
+                 .export = c("predict", "train", "trainControl"),
+                 .multicombine = TRUE,
+                 .errorhandling = "remove",
+                 .verbose = F
+) %dopar% {
+  # subset the data
+  subset.sample <- droplevels.data.frame(samples.melted[samples.melted$variable ==  traits[i],])
+  training <- subset.sample
+  trainControl <- caret::trainControl(method = "repeatedcv", number = 5, repeats = 10)
+
+  model_rf <- suppressWarnings(caret::train(x = training[, 2:3],
+                                     metric = "Rsquared",
+                                     y = training[, 5],
+                                     maximize = TRUE,
+                                     method = 'rf',
+                                     trControl = trainControl))
+
+    output <- data.frame(
+    plot.coord,
+    Trait = unique(subset.sample$variable),
+    Predicted.value = predict(object = model_rf, plot.coord[,2:3]),
+    Rsquared = round(as.numeric(model_rf$results[3]), 2),
+    RMSE = round(as.numeric(model_rf$results[2]), 3),
+    Samples = nrow(subset.sample)
+    )
+
+  }
+
+return(out.plot.level)
+
+}
